99a8b584cf4121014cba7e365b2a783ebf943a3c
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-22 15:14:46 -0600

    Finalized DAO Setups

---
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/daos/UserDAO.java
@@ -0,0 +1,32 @@
+package com.revature.reimbursementSystem.daos;
+
+import com.revature.reimbursementSystem.models.User;
+
+import java.util.List;
+
+public class UserDAO implements CrudDAO<User>{
+    @Override
+    public void save(User obj) {
+
+    }
+
+    @Override
+    public void delete(User obj) {
+
+    }
+
+    @Override
+    public void update(User obj) {
+
+    }
+
+    @Override
+    public User findById() {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+}
---
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/daos/UserRoleDAO.java
@@ -0,0 +1,32 @@
+package com.revature.reimbursementSystem.daos;
+
+import com.revature.reimbursementSystem.handlers.UserRole;
+
+import java.util.List;
+
+public class UserRoleDAO implements CrudDAO<UserRole>{
+    @Override
+    public void save(UserRole obj) {
+        
+    }
+
+    @Override
+    public void delete(UserRole obj) {
+
+    }
+
+    @Override
+    public void update(UserRole obj) {
+
+    }
+
+    @Override
+    public UserRole findById() {
+        return null;
+    }
+
+    @Override
+    public List<UserRole> findAll() {
+        return null;
+    }
+}
