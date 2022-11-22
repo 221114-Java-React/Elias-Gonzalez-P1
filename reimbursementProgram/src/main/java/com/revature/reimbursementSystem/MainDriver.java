c9ec4d7430520d70d74619216f9483ee63e08951
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-22 14:12:57 -0600

    RE-initialized Maven Project

---
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/MainDriver.java
@@ -0,0 +1,13 @@
+package com.revature.reimbursementSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainDriver
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/daos/CrudDAO.java
@@ -0,0 +1,4 @@
+package com.revature.reimbursementSystem.daos;
+
+public interface CrudDAO {
+}
