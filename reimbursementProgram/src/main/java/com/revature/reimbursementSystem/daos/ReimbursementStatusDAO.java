b98b818fd4fbf1966d4428b92f4db715c31f3cca
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-22 15:11:26 -0600

    Update ReimbursementStatusDAO.java

--- reimbursementProgram/src/main/java/com/revature/reimbursementSystem/daos/ReimbursementStatusDAO.java
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/daos/ReimbursementStatusDAO.java
@@ -1,4 +1,33 @@
 package com.revature.reimbursementSystem.daos;
 
-public class ReimbursementStatusDAO {
+import com.revature.reimbursementSystem.models.ReimbursementStatus;
+
+import java.util.List;
+
+public class ReimbursementStatusDAO implements CrudDAO<ReimbursementStatus> {
+
+    @Override
+    public void save(ReimbursementStatus obj) {
+
+    }
+
+    @Override
+    public void delete(ReimbursementStatus obj) {
+
+    }
+
+    @Override
+    public void update(ReimbursementStatus obj) {
+
+    }
+
+    @Override
+    public ReimbursementStatus findById() {
+        return null;
+    }
+
+    @Override
+    public List<ReimbursementStatus> findAll() {
+        return null;
+    }
 }
