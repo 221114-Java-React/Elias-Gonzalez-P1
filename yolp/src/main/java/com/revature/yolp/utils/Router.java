f5422d239198d29d1fb9939a031e412cba9452ae
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-21 22:46:32 -0600

    Update Router.java

--- yolp/src/main/java/com/revature/yolp/utils/Router.java
+++ yolp/src/main/java/com/revature/yolp/utils/Router.java
@@ -1,22 +1,29 @@
 package com.revature.yolp.utils;
 
-
+import com.revature.yolp.daos.UserDAO;
 import com.revature.yolp.handlers.UserHandler;
+import com.revature.yolp.services.UserService;
 import io.javalin.Javalin;
 
 import static io.javalin.apibuilder.ApiBuilder.path;
 import static io.javalin.apibuilder.ApiBuilder.post;
 
+//router class maps endpoints
 public class Router {
-    //purpose of router class is to map endpoints
-    public static void router(Javalin app){
-        //user
-        UserHandler userHandler = new UserHandler();
-        app.routes(() ->{
-            //handler groups
-            path("/users", ()->{
-                post(c-> userHandler.signup(c));
-                
+    public static void router(Javalin app) {
+        // User
+        UserDAO userDAO = new UserDAO();
+        UserService userService = new UserService(userDAO);
+        UserHandler userHandler = new UserHandler(userService);
+
+        // Restaurant
+
+        //Review
+
+        // handler groups
+        app.routes(() -> {
+            path("/users", () -> {
+                post(c -> userHandler.signup(c));
             });
         });
     }
