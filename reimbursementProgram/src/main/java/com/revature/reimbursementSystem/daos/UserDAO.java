6398ecd2a662e1f1a824ee2c1b0bd469388d120a
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-26 17:07:53 -0600

    Update UserDAO.java

--- reimbursementProgram/src/main/java/com/revature/reimbursementSystem/daos/UserDAO.java
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/daos/UserDAO.java
@@ -1,9 +1,10 @@
 package com.revature.reimbursementSystem.daos;
 
 import com.revature.reimbursementSystem.models.User;
-
 import java.util.List;
 
+/*Purpose of a DAO is to return data from the database
+* DAO = DATA ACCESS OBJECT*/
 public class UserDAO implements CrudDAO<User>{
     @Override
     public void save(User obj) {
