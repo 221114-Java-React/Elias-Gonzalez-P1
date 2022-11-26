17c3761b9124a624afb0dfb2654083db25f87d79
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-26 17:09:10 -0600

    Update MainDriver.java

--- reimbursementProgram/src/main/java/com/revature/reimbursementSystem/MainDriver.java
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/MainDriver.java
@@ -1,5 +1,6 @@
 package com.revature.reimbursementSystem;
 
+import com.revature.reimbursementSystem.utils.Router;
 import io.javalin.Javalin;
 
 /**
@@ -8,9 +9,14 @@ import io.javalin.Javalin;
  */
 public class MainDriver {
     public static void main(String[] args) {
+        //port number
         int port = 8080;
+        //instance of javalin to make endpoint "reimbursementSystem"
         Javalin app = Javalin.create(c -> {
             c.contextPath = "/reimbursementSystem";
         }).start(port);
+        /*router takes the Javalin object "app" and its endpoint
+        "/reimbursementSystem" and maps more endpoints.*/
+        Router.router(app);
     }
 }
