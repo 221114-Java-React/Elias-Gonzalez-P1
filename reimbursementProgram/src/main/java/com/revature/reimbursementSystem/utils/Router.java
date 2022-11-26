bb62a9001d96cdc5c4458279da532713034746d9
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-26 17:10:29 -0600

    Adds skeleton to router class

--- reimbursementProgram/src/main/java/com/revature/reimbursementSystem/utils/Router.java
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/utils/Router.java
@@ -1,5 +1,29 @@
 package com.revature.reimbursementSystem.utils;
 
-public class Router {
+import com.revature.reimbursementSystem.daos.UserDAO;
+import com.revature.reimbursementSystem.handlers.UserHandler;
+import com.revature.reimbursementSystem.services.UserService;
+import io.javalin.Javalin;
 
+import static io.javalin.apibuilder.ApiBuilder.path;
+import static io.javalin.apibuilder.ApiBuilder.post;
+
+//purpose of router classes is to map endpoints
+public class Router {
+    public static void router(Javalin app){
+        /*dependency injections*/
+        /*User*/
+        UserDAO userDAO = new UserDAO();
+        UserService userService = new UserService(userDAO);
+        UserHandler userhandler = new UserHandler(userService);
+        /*TODO: Add dependency injections for Reimbursements*/
+
+
+        /*handler groups*/
+        app.routes(()->{
+            path("/users",() -> {
+                post(c -> userhandler.signup(c));
+            });
+        });
+    }
 }
