3a61da9764ca345b1d0dbb2362a450388cf2ed03
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-26 15:55:31 -0600

    Update MainDriver.java
    Adds required main method and initializes javalin

--- reimbursementProgram/src/main/java/com/revature/reimbursementSystem/MainDriver.java
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/MainDriver.java
@@ -1,13 +1,16 @@
 package com.revature.reimbursementSystem;
 
+import io.javalin.Javalin;
+
 /**
  * Hello world!
  *
  */
-public class MainDriver
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class MainDriver {
+    public static void main(String[] args) {
+        int port = 8080;
+        Javalin app = Javalin.create(c -> {
+            c.contextPath = "/reimbursementSystem";
+        }).start(port);
     }
 }
