d5e4692ea016cdbd745aa577daec7b0715aaff7a
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-27 15:47:05 -0600

    Adds UserDAO.save() functionality
    leverage PS to perpare sql statement for DB query.

--- reimbursementProgram/src/main/java/com/revature/reimbursementSystem/daos/UserDAO.java
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/daos/UserDAO.java
@@ -1,6 +1,11 @@
 package com.revature.reimbursementSystem.daos;
 
 import com.revature.reimbursementSystem.models.User;
+import com.revature.reimbursementSystem.utils.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 /*Purpose of a DAO is to return data from the database
@@ -8,7 +13,19 @@ import java.util.List;
 public class UserDAO implements CrudDAO<User>{
     @Override
     public void save(User obj) {
-
+        try(Connection con = ConnectionFactory.getInstance().getConnection()){
+            PreparedStatement ps = con.prepareStatement("INSERT INTO, ers_users (user_id, username, email, password, given_name, surname, is_active, role_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?) )");
+            ps.setString(1, obj.getUser_id());
+            ps.setString(2, obj.getUsername());
+            ps.setString(3, obj.getEmail());
+            ps.setString(4, obj.getPassword());
+            ps.setString(5, obj.getGiven_name());
+            ps.setString(6, obj.getSurname());
+            ps.setString(7, obj.getRole_id());
+            ps.executeUpdate();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
