4ae60c33d40eda7887dde5e720b5e4d80913ffa0
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-27 15:45:42 -0600

    Create NewUserRequest.java

---
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/dtos/requests/NewUserRequest.java
@@ -0,0 +1,8 @@
+package com.revature.reimbursementSystem.dtos.requests;
+
+
+public class NewUserRequest {
+    private String username;
+    private String password;
+    private String password2;
+}
