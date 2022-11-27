25be6b7e4d2959b4c3b0d079afcfb743e35cba8b
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-27 17:02:46 -0600

    Add validation logic to UserService.saveUser()
    Add validation logic to prevent issues in submits to SQL db

--- reimbursementProgram/src/main/java/com/revature/reimbursementSystem/services/UserService.java
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/services/UserService.java
@@ -1,6 +1,12 @@
 package com.revature.reimbursementSystem.services;
 
 import com.revature.reimbursementSystem.daos.UserDAO;
+import com.revature.reimbursementSystem.dtos.requests.NewUserRequest;
+import com.revature.reimbursementSystem.models.User;
+import com.revature.reimbursementSystem.utils.customExceptions.InvalidUserException;
+
+import java.util.List;
+import java.util.UUID;
 
 /*services validate and retrieve data from DAO (data access objects)*/
 //service class is essentially an api
@@ -11,5 +17,27 @@ public class UserService {
     public UserService(UserDAO userDAO) {
         this.userDAO = userDAO;
     }
+    public void saveUser(NewUserRequest req){
+        List<String> usernames = userDAO.findAllUsernames();
+        List<String> emails = userDAO.findAllEmails();
+        if (!isValidUsername(req.getUsername())) throw new InvalidUserException("Username needs to be 8-20 characters long");
+        if(usernames.contains(req.getUsername())) throw new InvalidUserException("Username cannot already exist");
+        if (!isValidPassword(req.getPassword1())) throw new InvalidUserException("Password needs to be minimum eight characters long and at least one character and one number");
+        if (!isValidEmail(req.getEmail())) throw new InvalidUserException("Invalid email");
+        if (emails.contains((req.getEmail()))) throw new InvalidUserException("Email already exists");
+        if (!req.getPassword1().equals(req.getPassword2())) throw new InvalidUserException("Passwords do not match.");
+        //after all checks are done
+        User createdUser = new User(UUID.randomUUID().toString(),req.getUsername(), req.getEmail(), req.getPassword1(), req.getGiven_name(), req.getSurname(), "true", "DEFAULT");
+        userDAO.save(createdUser);
+    }
 
+    private boolean isValidUsername(String username){
+        return username.matches("^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$");
+    }
+    private boolean isValidPassword(String password){
+        return password.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");
+    }
+    private boolean isValidEmail(String email){
+        return email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+    }
 }
