efb3639a92a6621647705cc5f329be602dc5dae4
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-27 20:46:22 -0600

    Add Object Mapper to Router

--- reimbursementProgram/src/main/java/com/revature/reimbursementSystem/utils/Router.java
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/utils/Router.java
@@ -1,5 +1,6 @@
 package com.revature.reimbursementSystem.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.reimbursementSystem.daos.UserDAO;
 import com.revature.reimbursementSystem.handlers.UserHandler;
 import com.revature.reimbursementSystem.services.UserService;
@@ -13,9 +14,10 @@ public class Router {
     public static void router(Javalin app){
         /*dependency injections*/
         /*User*/
+        ObjectMapper mapper = new ObjectMapper();
         UserDAO userDAO = new UserDAO();
         UserService userService = new UserService(userDAO);
-        UserHandler userhandler = new UserHandler(userService);
+        UserHandler userhandler = new UserHandler(userService, mapper);
         /*TODO: Add dependency injections for Reimbursements*/
 
 
