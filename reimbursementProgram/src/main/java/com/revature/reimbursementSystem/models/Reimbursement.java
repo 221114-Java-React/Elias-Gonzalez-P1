d756d1995723c6ad299ffad0459c3909e51602fd
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-29 13:31:27 -0600

    Add reimbursement skeleton

--- reimbursementProgram/src/main/java/com/revature/reimbursementSystem/models/Reimbursement.java
+++ reimbursementProgram/src/main/java/com/revature/reimbursementSystem/models/Reimbursement.java
@@ -1,4 +1,6 @@
 package com.revature.reimbursementSystem.models;
 
 public class Reimbursement {
+    private String reimb_id, description, payment_id, author_id, resolver_id, status_id, type_id;
+
 }
